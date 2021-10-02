fc42d26c15137d6c1926187102bc220ba1f616c6
David Barrow <dave@fusion-arc.uk>
2021-10-02 14:38:05 +0100

    scaffold basic folder layout

--- main/settings.py
+++ main/settings.py
@@ -42,7 +42,9 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,6 +97,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # os.path.join(BASE_DIR, 'static')
+]
+
+# STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+## MEDIA_ROOT = BASE_DIR / "media" / "uploads"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
