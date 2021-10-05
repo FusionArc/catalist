6b4b0ee579bbe4347fe4b5cbf52e50c56dff3619
David Barrow <dave@fusion-arc.uk>
2021-10-05 11:00:54 +0100

    Update

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
+from .models import Asset, Brand, Maker
 # Register your models here.
--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
-class Asset(models.Model):
-    cat_id = models.CharField(max_length=200)
 
 class Brand(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        str(self.name)
+
 
 class Maker(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+       return str(self.name)
+
+
+class Asset(models.Model):
+    cat_id = models.CharField(max_length=200, unique=True)
+    brand = models.ForeignKey(Brand, default='UNKNOWN', on_delete=models.SET_DEFAULT)
+    maker = models.ForeignKey(Maker, default='UNKNOWN', on_delete=models.SET_DEFAULT)
+    name = f'{brand}_{str(id)}'
+
+    def __str__(self):
+       return str(self.name)
